8e53e95dfe5550d189f0a2b89c697681753c689a
redbeanicy <117802428+redbeanicy@users.noreply.github.com>
2022-11-09 15:05:51 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,13 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (R == receivedNumber) {
-        basic.showString("DRAW")
-    } else if (R == 0) {
-        if (receivedNumber == 1) {
-            basic.showString("U LOST")
-        } else {
-            basic.showString("U WON")
-        }
-    } else if (R == 1) {
-        if (receivedNumber == 2) {
-            basic.showString("U LOST")
-        } else {
-            basic.showString("U WON")
-        }
-    } else {
-        if (receivedNumber == 1) {
-            basic.showString("U LOST")
-        } else {
-            basic.showString("U WON")
-        }
-    }
+    Friend = receivedNumber
 })
 input.onGesture(Gesture.Shake, function () {
-    R = randint(0, 2)
-    radio.sendNumber(R)
-    if (R == 0) {
+    Me = randint(0, 2)
+    radio.sendNumber(Me)
+    if (Me == 0) {
         basic.showIcon(IconNames.Scissors)
         basic.pause(1000)
-    } else if (R == 1) {
+    } else if (Me == 1) {
         basic.showIcon(IconNames.SmallSquare)
         basic.pause(1000)
     } else {
@@ -35,5 +15,6 @@ input.onGesture(Gesture.Shake, function () {
         basic.pause(1000)
     }
 })
-let R = 0
+let Me = 0
+let Friend = 0
 radio.setGroup(1)
