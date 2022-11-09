ac3639f245ba093765d1bb2a3b3d167ebf2a65e5
redbeanicy <117802428+redbeanicy@users.noreply.github.com>
2022-11-09 16:16:35 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,49 +2,48 @@ radio.onReceivedNumber(function (receivedNumber) {
     Friend = receivedNumber
 })
 input.onGesture(Gesture.Shake, function () {
-    Me = randint(0, 2)
+    Me = randint(1, 3)
     radio.sendNumber(Me)
-    if (Me == 0) {
-        basic.showIcon(IconNames.Scissors)
-        basic.pause(1000)
-    } else if (Me == 1) {
-        basic.showIcon(IconNames.SmallSquare)
-        basic.pause(1000)
-    } else {
-        basic.showIcon(IconNames.Square)
-        basic.pause(1000)
-    }
 })
 let Friend = 0
 let Me = 0
 radio.setGroup(1)
-Me = 3
-Friend = 3
+Me = 0
+Friend = 0
 basic.forever(function () {
-    if (Me != 3 && Friend != 3) {
+    if (Me == 1) {
+        basic.showIcon(IconNames.Scissors)
+        basic.pause(1000)
+    } else if (Me == 2) {
+        basic.showIcon(IconNames.SmallSquare)
+        basic.pause(1000)
+    } else if (Me == 3) {
+        basic.showIcon(IconNames.Square)
+        basic.pause(1000)
+    }
+    if (Me != 0 && Friend != 0) {
         if (Me == Friend) {
             basic.showIcon(IconNames.Asleep)
-        } else if (Me == 0) {
-            if (Friend == 1) {
-                basic.showIcon(IconNames.Sad)
-                basic.showString("U LOST")
-            } else {
-                basic.showIcon(IconNames.Happy)
-                basic.showString("U WON")
-            }
         } else if (Me == 1) {
             if (Friend == 2) {
                 basic.showIcon(IconNames.Sad)
-                basic.showString("U LOST")
             } else {
-                basic.showString("U WON")
+                basic.showIcon(IconNames.Happy)
+            }
+        } else if (Me == 2) {
+            if (Friend == 3) {
+                basic.showIcon(IconNames.Sad)
+            } else {
+                basic.showIcon(IconNames.Happy)
             }
         } else {
-            if (Friend == 1) {
-                basic.showString("U LOST")
+            if (Friend == 2) {
+                basic.showIcon(IconNames.Sad)
             } else {
-                basic.showString("U WON")
+                basic.showIcon(IconNames.Happy)
             }
         }
+        Me = 0
+        Friend = 0
     }
 })
