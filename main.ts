5d6bd4b877804910c5fb7fe2530e8fb4e1a28090
redbeanicy <117802428+redbeanicy@users.noreply.github.com>
2022-11-09 15:38:45 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -15,6 +15,36 @@ input.onGesture(Gesture.Shake, function () {
         basic.pause(1000)
     }
 })
-let Me = 0
 let Friend = 0
+let Me = 0
 radio.setGroup(1)
+Me = 3
+Friend = 3
+basic.forever(function () {
+    if (Me != 3 && Friend != 3) {
+        if (Me == Friend) {
+            basic.showIcon(IconNames.Asleep)
+        } else if (Me == 0) {
+            if (Friend == 1) {
+                basic.showIcon(IconNames.Sad)
+                basic.showString("U LOST")
+            } else {
+                basic.showIcon(IconNames.Happy)
+                basic.showString("U WON")
+            }
+        } else if (Me == 1) {
+            if (Friend == 2) {
+                basic.showIcon(IconNames.Sad)
+                basic.showString("U LOST")
+            } else {
+                basic.showString("U WON")
+            }
+        } else {
+            if (Friend == 1) {
+                basic.showString("U LOST")
+            } else {
+                basic.showString("U WON")
+            }
+        }
+    }
+})
