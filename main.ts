c6d810b0608c483772e4da506f95277e61fe717c
redbeanicy <117802428+redbeanicy@users.noreply.github.com>
2022-11-10 19:57:35 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,49 +1,16 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    Friend = receivedNumber
-})
 input.onGesture(Gesture.Shake, function () {
     Me = randint(1, 3)
-    radio.sendNumber(Me)
-})
-let Friend = 0
-let Me = 0
-radio.setGroup(1)
-Me = 0
-Friend = 0
-basic.forever(function () {
     if (Me == 1) {
         basic.showIcon(IconNames.Scissors)
-        basic.pause(1000)
+        basic.pause(500)
     } else if (Me == 2) {
         basic.showIcon(IconNames.SmallSquare)
-        basic.pause(1000)
+        basic.pause(500)
     } else if (Me == 3) {
         basic.showIcon(IconNames.Square)
-        basic.pause(1000)
+        basic.pause(500)
     }
-    if (Me != 0 && Friend != 0) {
-        if (Me == Friend) {
-            basic.showIcon(IconNames.Asleep)
-        } else if (Me == 1) {
-            if (Friend == 2) {
-                basic.showIcon(IconNames.Sad)
-            } else {
-                basic.showIcon(IconNames.Happy)
-            }
-        } else if (Me == 2) {
-            if (Friend == 3) {
-                basic.showIcon(IconNames.Sad)
-            } else {
-                basic.showIcon(IconNames.Happy)
-            }
-        } else {
-            if (Friend == 2) {
-                basic.showIcon(IconNames.Sad)
-            } else {
-                basic.showIcon(IconNames.Happy)
-            }
-        }
-        Me = 0
-        Friend = 0
-    }
+    basic.clearScreen()
 })
+let Me = 0
+radio.setGroup(1)
